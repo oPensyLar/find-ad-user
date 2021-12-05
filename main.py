d7e866a261692eb54804e6052df357b31f0161c2
oPensyLar <vmgomez.dev@protonmail.com>
2021-12-05 13:48:38 -0400

    remove file

--- main.py
+++ main.py
@@ -15,30 +15,44 @@ def check_user_process(wmi_obj, user):
     return False
 
 
-ipaddr = "127.0.0.1"
 user = "you-ad-user"
 password = "you-ad-password"
 user_target = "ad-user-to-find"
+file_servers = "srv.txt"
+file_users = "users-to-find.txt"
 
-user_found = False
-ssh = ssh_client.SshClient(ipaddr, 22, user, password)
-ret_data = ssh.ssh_exec("ps uax")
+with open(file_servers, "r") as handle_servers:
+    for c_addr in handle_servers:
+        c_addr = c_addr.replace("\n", "")
+        c_addr = c_addr.replace("\r", "")
 
-if ret_data is None:
-    try:
-        # Is Windows host?
-        wmi_object = wmi.WMI(ipaddr, user=user, password=password)
-        user_found = check_user_process(wmi_object, user_target)
+        with open(file_users, "r") as handle_user:
+            for c_user in handle_user:
+                c_user = c_user.replace("\n", "")
+                c_user = c_user.replace("\r", "")
 
-    except wmi.x_access_denied:
-        print("[!] auth Windows access denied")
+                user_found = False
+                ssh = ssh_client.SshClient(c_addr, 22, user, password)
+                ret_data = ssh.ssh_exec("ps uax")
 
-else:
-    p = _parser.Parser()
-    user_found = p.parse_procs(ret_data["stdout"], user_target)
+                if ret_data is None:
+                    try:
+                        # Is Windows host?
+                        wmi_object = wmi.WMI(c_addr, user=user, password=password)
+                        user_found = check_user_process(wmi_object, user_target)
 
-if user_found is True:
-    headers = ["username", "logged_on"]
-    data = [[user_target, ipaddr]]
-    csv = csv_writer.CsvWriter("output.csv", headers, data)
-    csv.create_csv()
+                    except wmi.x_access_denied:
+                        print("[!] auth Windows access denied on " + c_addr)
+
+                else:
+                    if ret_data is None:
+                        continue
+
+                    p = _parser.Parser()
+                    user_found = p.parse_procs(ret_data["stdout"], user_target)
+
+            if user_found is True:
+                headers = ["username", "logged_on"]
+                data = [[user_target, c_addr]]
+                csv = csv_writer.CsvWriter("output.csv", headers, data)
+                csv.create_csv()
