e3104aa87849b4867319f798db3dc8c14ddc4fe5
oPensyLar <vmgomez.dev@protonmail.com>
2021-12-07 17:57:29 -0400

    Linux support dropped

--- main.py
+++ main.py
@@ -1,60 +1,70 @@
 import wmi
-import ssh_client
 import _parser
 import csv_writer
+import pyad.adquery
+
+enable_ssh_find = False
+
+if enable_ssh_find:
+	import ssh_client
+
+def check_user_process(wmi_obj, user):	
+	for s in wmi_obj.Win32_Process():
+		owner = s.GetOwner()
+
+		if owner[0] is not None:
+			index=owner[2].find(user)
+			if index >= 0x0:
+				return {"found": True, "user": owner[2]}
+
+	return {"found": False, "user": None}
 
 
-def check_user_process(wmi_obj, user):
-    for s in wmi_obj.Win32_Process():
-        owner = s.GetOwner()
-
-        if owner[0] is not None:
-            if owner[2] == user:
-                return True
-
-    return False
-
-
-user = "you-ad-user"
-password = "you-ad-password"
-file_servers = "srv.txt"
+user = ""
+password = ""
 file_users = "users-to-find.txt"
 
-with open(file_servers, "r") as handle_servers:
-    for c_addr in handle_servers:
-        c_addr = c_addr.replace("\n", "")
-        c_addr = c_addr.replace("\r", "")
+q = pyad.adquery.ADQuery()
+q.execute_query(attributes=["distinguishedName", "description"], where_clause="objectClass = 'computer'")
 
-        with open(file_users, "r") as handle_user:
-            for c_user in handle_user:
-                c_user = c_user.replace("\n", "")
-                c_user = c_user.replace("\r", "")
+for row in q.get_results():
+    str_split = row["distinguishedName"].split(",")
+    str_split = str_split[0].split("=")
+    print("Checking on " + str_split[1])
 
-                user_found = False
-                ssh = ssh_client.SshClient(c_addr, 22, user, password)
-                ret_data = ssh.ssh_exec("ps uax")
+    with open(file_users, "r") as handle_user:
+    	ret_data = None
+    	for c_user in handle_user:
+            c_user = c_user.replace("\n", "")
+            c_user = c_user.replace("\r", "")
+
+            if enable_ssh_find:
+            	user_found = False
+            	ssh = ssh_client.SshClient(str_split[1], 22, user, password)
+            	ret_data = ssh.ssh_exec("ps uax")
+            	p = _parser.Parser()
+            	user_found = p.parse_procs(ret_data["stdout"], c_user)
+
+            if ret_data is None:
+                try:
+                    # Is Windows host?
+                    wmi_object = wmi.WMI(str_split[1], user=user, password=password)
+                    user_found = check_user_process(wmi_object, c_user)
+
+                    if user_found["found"] is True:
+                    	headers = ["username", "logged_on"]
+                    	data = [[user_found["user"], str_split[1]]]
+                    	csv = csv_writer.CsvWriter("output.csv", headers, data)
+                    	csv.create_csv()                    
+
+                except wmi.x_wmi:
+                    print("[!] Host is down? on " + str_split[1] + "\n")
+
+                except wmi.x_access_denied:
+                    print("[!] x_access_denied on " + str_split[1])
+
+                except wmi.x_wmi_authentication:
+                    print("[!] x_wmi_authentication on " + str_split[1])
 
                 if ret_data is None:
-                    try:
-                        # Is Windows host?
-                        wmi_object = wmi.WMI(c_addr, user=user, password=password)
-                        user_found = check_user_process(wmi_object, c_user)
-
-                    except wmi.x_access_denied:
-                        print("[!] x_access_denied on " + c_addr)
-
-                    except wmi.x_wmi_authentication:
-                        print("[!] x_wmi_authentication on " + c_addr)
-
-                else:
-                    if ret_data is None:
-                        continue
-
-                    p = _parser.Parser()
-                    user_found = p.parse_procs(ret_data["stdout"], c_user)
-
-            if user_found is True:
-                headers = ["username", "logged_on"]
-                data = [[user_target, c_addr]]
-                csv = csv_writer.CsvWriter("output.csv", headers, data)
-                csv.create_csv()
+                	continue
